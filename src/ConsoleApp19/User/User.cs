9546e8ed4c752505d7249f52005b0da18a8a0c16
dilmurod <abdurahimovdilmurod941@gmail.com>
2023-01-25 12:43:13 +0500

    Addeduser

---
+++ src/ConsoleApp19/User/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19.User
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}

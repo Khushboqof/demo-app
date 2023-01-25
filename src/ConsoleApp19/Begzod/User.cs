6f01e8fd12e1a0fc76f66ae807dec8a3132dca71
BegzodDev <begzodpublic@gmail.com>
2023-01-25 12:31:18 +0500

    Begzod User

---
+++ src/ConsoleApp19/Begzod/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19.Begzod
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SureName { get; set; }
+
+    }
+}

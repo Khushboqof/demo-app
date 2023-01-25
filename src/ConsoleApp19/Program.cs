ca22f6c667725588ca4a0daf80d25a6c41e1a736
davronbekxabibullayev <davronbekxabibyllayev03.06.88@gmail.com>
2023-01-25 12:56:58 +0500

    addaddOIntetwryeh678

--- src/ConsoleApp19/Program.cs
+++ src/ConsoleApp19/Program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+<<<<<<< HEAD
 var soo = 123435;
 Console.WriteLine(soo);
+=======
+fadbgsnfhdjmk,yh
+var drii = "Hello everyone";
+
+Console.WriteLine("My name is dilshod");

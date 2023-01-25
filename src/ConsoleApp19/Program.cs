5839b12421799476066085251619a5f9ad71bb3d
BegzodDev <begzodpublic@gmail.com>
2023-01-25 13:15:09 +0500

    6

--- src/ConsoleApp19/Program.cs
+++ src/ConsoleApp19/Program.cs
@@ -1,10 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
 var soo = 123435;
 Console.WriteLine(soo);
-=======
-fadbgsnfhdjmk,yh
+
 var drii = "Hello everyone";
 
 Console.WriteLine("My name is dilshod");
